dc26f9c69d29949f783b35e9b9867e1ec30d29df
JadwigaAdamiec <79207166+JadwigaAdamiec@users.noreply.github.com>
2022-11-07 23:01:30 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,6 +1,26 @@
 def is_prime(n):
-    return
+    if (n<2):
+        return False
+    else:
+        for i in range(n//2):
+            if (n%i == 0):
+                return False
+            else:
+                return True
 
 
 def is_diabolic(n):
-    return
+    x = str(n)
+    if ('666' in x):
+        return True
+
+
+def all_of_them():
+    numer = 0
+    for i in range(1, 10000):
+        if (is_prime(i) and is_diabolic(i)):
+            print(i)
+            number += 1
+    print("ilosc: " + numer)
+    
+all_of_them()
