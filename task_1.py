182cbabd08b7646195b78f4187c89f66675966ab
JadwigaAdamiec <79207166+JadwigaAdamiec@users.noreply.github.com>
2022-11-07 23:20:42 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,16 @@
 def remove_parentheses(text):
-    return text
+    result = ""
+    is_inside = False
+    for letter in text:
+        if(letter == '('):
+            is_inside = True
+            result += ''
+        elif(letter == ')'):
+            is_inside = False
+        elif not is_inside:
+            result += letter
+    return result
+            
+    
+text = "(Nie) jest tak zle"
+print (remove_parentheses(text))
