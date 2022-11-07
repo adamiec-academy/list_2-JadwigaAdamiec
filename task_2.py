2eb433a67ecb57d130b1e3799d5bae3e8277c560
JadwigaAdamiec <79207166+JadwigaAdamiec@users.noreply.github.com>
2022-11-07 22:12:47 +0100

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,1 +1,26 @@
-# Remove this comment to confirm that this task is done
+from turtle import penup, pendown, forward, left, right
+from random import randint 
+
+def draw_rectangle(n):
+  a = randint(20,200)
+  forward(a)
+  right(90)
+  forward(n)
+  right(90)
+  forward(a)
+  right(90)
+  forward(n)
+  penup()
+  right(180)
+  forward(n+10)
+  pendown()
+  left(90)
+
+
+penup()
+left(180)
+forward(150)
+right(90)
+pendown()
+for i in range(20):
+  draw_rectangle(15)
